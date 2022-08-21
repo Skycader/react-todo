0f445881466fd04c2eb2ff623a69edb2d3659ff9
skycader <skycader@github.com>
2022-08-21 16:36:48 +0300

    console.log removed

---
+++ build/precache-manifest.a7c7d5c8dfeaab5f751d0d23b23cd3a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b51ad226721a65134df",
+    "url": "/react-todo/static/css/main.a09b630d.chunk.css"
+  },
+  {
+    "revision": "2b51ad226721a65134df",
+    "url": "/react-todo/static/js/main.21cbfdb2.chunk.js"
+  },
+  {
+    "revision": "713502522859ff23d42e",
+    "url": "/react-todo/static/js/runtime~main.81d0ab0d.js"
+  },
+  {
+    "revision": "54f1b60512d53702c847",
+    "url": "/react-todo/static/css/2.5a37ff42.chunk.css"
+  },
+  {
+    "revision": "54f1b60512d53702c847",
+    "url": "/react-todo/static/js/2.3022a969.chunk.js"
+  },
+  {
+    "revision": "7c21e1030e4d3777e0717afbf167154c",
+    "url": "/react-todo/index.html"
+  }
+];
